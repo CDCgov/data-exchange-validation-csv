ece9f38f3f473307f107b742fee99ed97a3fc458
Peterson <jacob.r.peterson@accenturefederal.com>
2023-07-14 13:46:57 -0500

    fixing some bugs in the split compare project

--- fns-csv-pipeline/src/main/kotlin/gov/cdc/dex/csv/functions/DummyFileSplitCompare.kt
+++ fns-csv-pipeline/src/main/kotlin/gov/cdc/dex/csv/functions/DummyFileSplitCompare.kt
@@ -1,6 +1,7 @@
 package gov.cdc.dex.csv.functions
 
 import com.azure.storage.blob.BlobClientBuilder
+import com.azure.core.util.BinaryData
 import com.microsoft.azure.functions.annotation.FunctionName
 import com.microsoft.azure.functions.annotation.HttpTrigger
 import com.microsoft.azure.functions.annotation.AuthorizationLevel
@@ -15,6 +16,7 @@ import com.microsoft.durabletask.Task
 import com.microsoft.durabletask.TaskOrchestrationContext
 import com.microsoft.durabletask.azurefunctions.DurableOrchestrationTrigger
 import com.microsoft.durabletask.azurefunctions.DurableActivityTrigger;
+import com.fasterxml.jackson.annotation.JsonProperty
 
 import java.util.Optional
 import java.util.concurrent.Executors
@@ -26,6 +28,8 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
+import java.time.format.DateTimeFormatter
+import kotlin.random.Random
 
 class DummyFileSplitCompare {
     private fun commonOpenFileStream(blobUrl:String):InputStream{
@@ -35,7 +39,8 @@ class DummyFileSplitCompare {
         return client.openInputStream()
     }
 
-    private fun commonProcessBatch(batchContent:String):String{
+    private fun commonProcessBatch(batchContent:String, delayTime:Long):String{
+        Thread.sleep(delayTime)
         return batchContent.split("\n").map{it.first()}.fold(""){agg,element -> agg+element}
     }
     
@@ -52,6 +57,7 @@ class DummyFileSplitCompare {
             val startTime = LocalDateTime.now()
             val batchSize = request.queryParameters["batchSize"]?.toInt()?:1
             val numberThreads = request.queryParameters["numberThreads"]?.toInt()?:10
+            val delayTime = request.queryParameters["delayTime"]?.toLong()?:0
             val fileUrl = request.body.get()
             val inputStream = commonOpenFileStream(fileUrl)
             
@@ -70,7 +76,7 @@ class DummyFileSplitCompare {
                     stringBuilder.append("\n").append(nextLine);
                     if(currentInBatch >= batchSize) {
                         val batchSubmit = headerLine + stringBuilder.toString()
-                        futures.add(service.submit(Callable<String>{commonProcessBatch(batchSubmit)}))
+                        futures.add(service.submit(Callable<String>{commonProcessBatch(batchSubmit, delayTime)}))
                         
                         stringBuilder.clear()
                         currentInBatch = 0
@@ -79,7 +85,7 @@ class DummyFileSplitCompare {
                 }
                 if(stringBuilder.isNotEmpty()){
                     val batchSubmit = headerLine + stringBuilder.toString()
-                    futures.add(service.submit(Callable<String>{commonProcessBatch(batchSubmit)}))
+                    futures.add(service.submit(Callable<String>{commonProcessBatch(batchSubmit, delayTime)}))
                 }
             }
         
@@ -113,9 +119,11 @@ class DummyFileSplitCompare {
             context.logger.log(Level.INFO,"Dummy_Multi_Router started")
             val startTime = LocalDateTime.now()
             val batchSize = request.queryParameters["batchSize"]?.toInt()?:1
+            val numberThreads = request.queryParameters["numberThreads"]?.toInt()?:10
+            val delayTime = request.queryParameters["delayTime"]?.toLong()?:0
             val fileUrl = request.body.get()
 
-            val orchInput = DumOrchInput(batchSize, fileUrl, startTime.toString())
+            val orchInput = DumOrchInput(batchSize, numberThreads, delayTime, fileUrl, startTime.toString())
             
             val instanceId = durableContext.client.scheduleNewOrchestrationInstance("Dummy_Multi_Orchestrator",orchInput);
 
@@ -134,58 +142,100 @@ class DummyFileSplitCompare {
         @DurableOrchestrationTrigger(name = "taskOrchestrationContext") taskContext:TaskOrchestrationContext,
         functionContext:ExecutionContext
     ):DumOrchOutput  {
-        orchlog(Level.INFO,"Dummy_Multi_Orchestrator started", taskContext=taskContext, functionContext=functionContext)
-        val input = taskContext.getInput(DumOrchInput::class.java)
         try{
-
-            val inputStream = commonOpenFileStream(input.fileUrl)
+            orchlog(Level.INFO,"Dummy_Multi_Orchestrator started", taskContext=taskContext, functionContext=functionContext)
+            val input = taskContext.getInput(DumOrchInput::class.java)
+            //functionContext.logger.info("HERE 0")
+            try{
+                val splitOutput = taskContext.callActivity("Dummy_Multi_Splitter", DumSplitInput(input), DumSplitOutput::class.java).await()
+                //functionContext.logger.info("HERE 1")
             
-            val parallelTasks:MutableList<Task<DumActivityOutput>> = mutableListOf()
-            BufferedReader(InputStreamReader(inputStream)).use{
-                
-                val headerLine = it.readLine()
-
-                var currentInBatch = 0
-                var stringBuilder = StringBuilder()
-                
-                var nextLine = it.readLine()
-                while(nextLine != null) {
-                    currentInBatch++;
-                    stringBuilder.append("\n").append(nextLine);
-                    if(currentInBatch >= input.batchSize) {
-                        val batchSubmit = headerLine + stringBuilder.toString()
-                        parallelTasks.add(taskContext.callActivity("Dummy_Multi_Activity", DumActivityInput(batchSubmit), DumActivityOutput::class.java))
-
-                        stringBuilder.clear()
-                        currentInBatch = 0
-                    }
-                    nextLine = it.readLine()
+                val parallelTasks:MutableList<Task<DumActivityOutput>> = mutableListOf()
+                for(batchKey in splitOutput.batchKeyList){
+                    parallelTasks.add(taskContext.callActivity("Dummy_Multi_Activity", DumActivityInput(batchKey, input), DumActivityOutput::class.java))
                 }
-                if(stringBuilder.isNotEmpty()){
-                    val batchSubmit = headerLine + stringBuilder.toString()
-                    parallelTasks.add(taskContext.callActivity("Dummy_Multi_Activity", DumActivityInput(batchSubmit), DumActivityOutput::class.java))
-                }
-
+                //functionContext.logger.info("HERE 2")
+            
+                orchlog(Level.INFO,"Dummy_Multi_Orchestrator waiting for tasks", taskContext=taskContext, functionContext=functionContext)
+                
+                val subOutputs = taskContext.allOf(parallelTasks).await()
+                //functionContext.logger.info("HERE 3")
+                val out = subOutputs.map{it.outputMessage}.toString()
+                //functionContext.logger.info("HERE 4")
+            
+                val startTime = LocalDateTime.parse(input.startTime)
+                //functionContext.logger.info("HERE 5")
+            
+                val endTime = LocalDateTime.now()
+                //functionContext.logger.info("HERE 6")
+                val timeDiff = ChronoUnit.MILLIS.between(startTime, endTime)
+                orchlog(Level.INFO,"Dummy_Multi_Orchestrator finished, time $timeDiff millis", taskContext=taskContext, functionContext=functionContext)
+                
+                return DumOrchOutput(timeDiff, splitOutput.timeDiffSplit, input, splitOutput, out, endTime.toString())
+                
+            }catch(e:com.microsoft.durabletask.OrchestratorBlockedException){
+                throw e
+            }catch(e:Exception){
+                orchlog(Level.SEVERE,"Dummy_Multi_Orchestrator error ${e.localizedMessage}",e, taskContext=taskContext, functionContext=functionContext)
+                return DumOrchOutput(null, null, input, null, "ERROR Dummy_Multi_Activity ${e.localizedMessage}", null)
             }
-
-            orchlog(Level.INFO,"Dummy_Multi_Orchestrator waiting for tasks", taskContext=taskContext, functionContext=functionContext)
-
-            val subOutputs = taskContext.allOf(parallelTasks).await()
-            val out = subOutputs.map{it.outputMessage}.toString()
-
-            val startTime = LocalDateTime.parse(input.startTime)
-            val endTime = LocalDateTime.now()
-            val timeDiff = ChronoUnit.MILLIS.between(startTime, endTime)
-            orchlog(Level.INFO,"Dummy_Multi_Orchestrator finished, time $timeDiff millis", taskContext=taskContext, functionContext=functionContext)
-            
-            return DumOrchOutput(input, out, endTime.toString())
-            
-        }catch(e:com.microsoft.durabletask.OrchestratorBlockedException){
-            throw e
         }catch(e:Exception){
-            orchlog(Level.SEVERE,"Dummy_Multi_Router error",e, taskContext=taskContext, functionContext=functionContext)
-            return DumOrchOutput(input, "ERROR ${e.localizedMessage}", LocalDateTime.now().toString())
+            functionContext.logger.log(Level.SEVERE,"ERROR ${e.localizedMessage}",e)
+            throw e
         }
+    }
+    
+    @FunctionName("Dummy_Multi_Splitter")
+    fun Dummy_Multi_Splitter(
+        @DurableActivityTrigger(name = "input") input: DumSplitInput, 
+        context: ExecutionContext 
+    ):DumSplitOutput{
+        context.logger.log(Level.INFO,"Dummy_Multi_Splitter started ${input}")
+        val startTime = LocalDateTime.now()
+        val inputStream = commonOpenFileStream(input.orchInput.fileUrl)
+        val batchSize = input.orchInput.batchSize
+        val numberThreads = input.orchInput.numberThreads
+        
+        val futures = mutableListOf<Future<String>>()
+        BufferedReader(InputStreamReader(inputStream)).use{
+            
+            val headerLine = it.readLine()
+            val service = Executors.newFixedThreadPool(numberThreads)
+            
+            var currentInBatch = 0
+            var stringBuilder = StringBuilder()
+            
+            var nextLine = it.readLine()
+            while(nextLine != null) {
+                currentInBatch++;
+                stringBuilder.append("\n").append(nextLine);
+                if(currentInBatch >= batchSize) {
+                    val batchSubmit = headerLine + stringBuilder.toString()
+                    futures.add(service.submit(Callable<String>{storeBatch(batchSubmit)}))
+                    
+                    stringBuilder.clear()
+                    currentInBatch = 0
+                }
+                nextLine = it.readLine()
+            }
+            if(stringBuilder.isNotEmpty()){
+                val batchSubmit = headerLine + stringBuilder.toString()
+                futures.add(service.submit(Callable<String>{storeBatch(batchSubmit)}))
+            }
+        }
+    
+    
+        var stillRunning = futures.size
+        while(stillRunning > 0) {
+            stillRunning = futures.filter{!it.isDone}.size
+        }
+
+        val batchKeyList = futures.map{it.get()}
+        
+        val endTime = LocalDateTime.now()
+        val timeDiff = ChronoUnit.MILLIS.between(startTime, endTime)
+
+        return DumSplitOutput(input, batchKeyList, timeDiff)
     }
     
     @FunctionName("Dummy_Multi_Activity")
@@ -193,12 +243,17 @@ class DummyFileSplitCompare {
         @DurableActivityTrigger(name = "input") input: DumActivityInput, 
         context: ExecutionContext 
     ):DumActivityOutput{
-        val out=commonProcessBatch(input.batchContent)
+        try{
+            val content = retrieveBatch(input.batchKey)
+            val out=commonProcessBatch(content, input.orchInput.delayTime)
+            return DumActivityOutput(input, out)
 
-        return DumActivityOutput(input, out)
+        }catch(e:Exception){
+            context.logger.log(Level.SEVERE,"ERROR",e)
+            throw e
+        }
+
     }
-
-    
 
     private fun orchlog(
         level:Level, msg:String, thrown:Throwable?=null, taskContext:TaskOrchestrationContext, functionContext:ExecutionContext
@@ -210,27 +265,62 @@ class DummyFileSplitCompare {
         } else{
             functionContext.logger.log(level, toLog, thrown);
         }
+    }
+
+    private fun storeBatch(content:String):String{
+
+        val connectionString = System.getenv("BlobConnection")
+        val timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss-SSSSSS"))
+        val rand = Random.nextInt(0,999)
+        val batchKey = "https://dexcsvdata001.blob.core.windows.net/processed/test-split/${timestamp}.R${rand}"
         
+        val client = BlobClientBuilder().connectionString(connectionString).endpoint(batchKey).buildClient()
+        client.blockBlobClient.upload(BinaryData.fromString(content))
+        return batchKey
+    }
+    
+    private fun retrieveBatch(batchKey:String):String{
+
+        val connectionString = System.getenv("BlobConnection")
+        
+        val client = BlobClientBuilder().connectionString(connectionString).endpoint(batchKey).buildClient()
+        return client.downloadContent().toString()
     }
 }
 
 data class DumOrchInput(
-    val batchSize       : Int,
-    val fileUrl         : String,
-    val startTime       : String
+    @JsonProperty("batchSize")      val batchSize       : Int,
+    @JsonProperty("numberThreads")  val numberThreads   : Int,
+    @JsonProperty("delayTime")      val delayTime       : Long,
+    @JsonProperty("fileUrl")        val fileUrl         : String,
+    @JsonProperty("startTime")      val startTime       : String
 )
 
 data class DumOrchOutput(
-    val intput          : DumOrchInput,
-    val outputMessage   : String,
-    val endTime         : String
+    @JsonProperty("timeDiff")       val timeDiff        : Long?,
+    @JsonProperty("timeDiffSplit")  val timeDiffSplit   : Long?,
+    @JsonProperty("input")          val input           : DumOrchInput,
+    @JsonProperty("splitOutput")    val splitOutput     : DumSplitOutput?,
+    @JsonProperty("outputMessage")  val outputMessage   : String,
+    @JsonProperty("endTime")        val endTime         : String?
+)
+
+data class DumSplitInput(
+    @JsonProperty("orchInput")      val orchInput       : DumOrchInput
+)
+
+data class DumSplitOutput(
+    @JsonProperty("input")          val input           : DumSplitInput,
+    @JsonProperty("batchKeyList")   val batchKeyList    : List<String>,
+    @JsonProperty("timeDiffSplit")  val timeDiffSplit   : Long
 )
 
 data class DumActivityInput(
-    val batchContent    : String
+    @JsonProperty("batchKey")       val batchKey        : String,
+    @JsonProperty("orchInput")      val orchInput       : DumOrchInput
 )
 
 data class DumActivityOutput(
-    val input           : DumActivityInput,
-    val outputMessage   : String
+    @JsonProperty("input")          val input           : DumActivityInput,
+    @JsonProperty("outputMessage")  val outputMessage   : String
 )
